87f802a344c4ddca56cdef7c7571deaaa3653034
Kis Erik Roland <kis.erik.roland@bot.local>
2022-11-25 08:47:06 +0100

    feggfr

--- Szamologep/Szamologep/Program.cs
+++ Szamologep/Szamologep/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Adj meg egy számot : ");
+
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Milyen műveletet szeretnél elvégezni? (+, -, *, /): ");
+
+char muvelet = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Adj meg egy másik számot: ");
+
+double b = Convert.ToDouble(Console.ReadLine());
+
+
+if (muvelet == '+')
+{
+
+    Console.WriteLine($"Az művelet eredménye: {a+b}");
+
+
+}
+else if (muvelet == '-')
+{
+
+    Console.WriteLine($"A művelet eredménye: {a - b }");
+
+
+}
+else if (muvelet == '*')
+{
+
+    Console.WriteLine($"A művelet eredménye: { a* b}");
+
+
+}
+
+if (muvelet == '/' && a != 0 && b!=0 )
+{
+
+    Console.WriteLine($"A művelet eredménye:  {a / b}");
+
+
+}
+else if (muvelet == '/' && a == 0  || b == 0) 
+{
+
+    Console.WriteLine("Nullával nem osztunk");
+
+}
+
+else
+{
+
+    Console.WriteLine("Nem értem");
+
+}
+
+
