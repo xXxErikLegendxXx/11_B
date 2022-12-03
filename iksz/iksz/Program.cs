9326e7f7d1afaee6a5ee3baf146e5d3fc171b1dc
Erikm0 <kis.erik.roland@blathy.info>
2022-12-03 16:44:35 +0100

    iksz

--- iksz/iksz/Program.cs
+++ iksz/iksz/Program.cs
@@ -8,10 +8,14 @@
             int x = 0;
             int yF = 0;
             int yL = Console.WindowHeight;
+
+            //x közepe (viszonyítási alapnak)
             int kozep_x = Console.WindowWidth / 2;
             int kozep_y = Console.WindowHeight / 2;
             Console.SetCursorPosition(kozep_x, kozep_y);
             Console.Write(iksz);
+
+            //Kiírja az x-et
 
             while (x != kozep_x * 2 && yF != kozep_y * 2)
             {
