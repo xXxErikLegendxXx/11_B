56e9bea273c7766d238c13ebc57d8b192f8db8a8
Erikm0 <kis.erik.roland@blathy.info>
2022-12-03 16:13:38 +0100

    iksz

---
+++ iksz/iksz/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace iksz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char iksz = 'x';
+            int x = 0;
+            int y = 0;
+            int kozep_x = Console.WindowWidth / 2;
+            int kozep_y = Console.WindowHeight / 2;
+            Console.SetCursorPosition(kozep_x, kozep_y);
+            Console.WriteLine(iksz);
+
+            while (x != kozep_x && y != kozep_y)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine(iksz);
+
+                x+= 4;
+                y++;
+            }
+        }
+    }
+}
