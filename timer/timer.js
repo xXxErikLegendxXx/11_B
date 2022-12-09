6cc642468031af938f31c408670e86e44a2a6e4c
Erikm0 <kis.erik.roland@blathy.info>
2022-12-09 12:42:44 +0100

    timer html javascript

--- timer/timer.js
+++ timer/timer.js
@@ -1,1 +1,24 @@
+const perc = 0.10;
 
+let masodperc = perc * 60;
+
+
+const csoken = setInterval(countdown,1000)
+
+function countdown(){
+
+    percb = Math.floor(masodperc / 60);
+    masodpercb = Math.floor(masodperc % 60);
+
+    masodpercb = masodpercb < 10 ? "0" + masodpercb: masodpercb;
+
+    document.getElementById("timer").innerHTML = `${percb}:${masodpercb}`
+    if(masodpercb < 1 ){
+
+        clearInterval(csoken)
+
+    }
+    masodperc--;
+
+
+}
