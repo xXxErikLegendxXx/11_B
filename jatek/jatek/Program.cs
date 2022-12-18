0456f1ad7fce83f91f3479aa7bdbd1eba0108537
Erikm0 <kis.erik.roland@blathy.info>
2022-12-18 16:32:41 +0100

    jatek

--- jatek/jatek/Program.cs
+++ jatek/jatek/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace jatek
 {
@@ -8,9 +9,88 @@ namespace jatek
         {
             //üdvözlés
 
-            Console.WriteLine("Narrátor: Üdvözlet utazó, mond, hogy hívnak?");
+            Console.WriteLine("Vendéglátó: Üdvözlet utazó, mond, hogy hívnak?");
             string nev = Convert.ToString(Console.ReadLine());
-            Console.WriteLine($"Narrátor: Örülök, hogy megismerhettelek {nev}");
+            Console.WriteLine($"Vendéglátó: Örülök, hogy megismerhettelek {nev}");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Vendéglátó: Mi szél hozott ide?");
+            Thread.Sleep(5000);
+
+            Console.WriteLine("Vendéglátó: Nem vagy a legbeszédesebb kedvedben");
+            Thread.Sleep(1000);
+            Console.WriteLine("Vendéglátó: Na, de nem is baj");
+            Thread.Sleep(1000);
+            Console.WriteLine("Vendéglátó: Melyik szobát szeretnéd választani 1-től 10-ig? ");
+
+            int szoba_szam = Convert.ToInt32(Console.ReadLine());
+
+            if (szoba_szam > 10)
+            {
+
+                Console.WriteLine("Vendéglátó: Az egyetlen probléma ezzel a választással, hogy az a szoba foglalt.");
+                Thread.Sleep(1000);
+                Console.WriteLine("Vendéglátó: Tehát, melyik szobát szeretnéd választani 1-től 10-ig? ");
+
+                szoba_szam = Convert.ToInt32(Console.ReadLine());
+
+                int lehetosegek = 0;
+
+                while (szoba_szam > 10)
+                {
+                    lehetosegek++;
+                    Console.WriteLine("Vendéglátó: Az egyetlen probléma ezzel a választással, hogy az a szoba foglalt.");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Vendéglátó: Tehát, melyik szobát szeretnéd választani 1-től 10-ig? ");
+
+                    szoba_szam = Convert.ToInt32(Console.ReadLine());
+
+
+
+                    if (lehetosegek == 2)
+                    {
+                        Console.WriteLine("Vendéglátó: Na jó, úgylátszik nem értetted a feladatot ezért én választom neked a 7-es szobát");
+                        szoba_szam = 7;
+                        break;
+                    }
+                }
+            }
+            else if (szoba_szam < 1)
+            {
+                Console.WriteLine("Vendéglátó: Az egyetlen probléma ezzel a választással, hogy az a szoba nem létezik.");
+                Thread.Sleep(1000);
+                Console.WriteLine("Vendéglátó: Tehát, melyik szobát szeretnéd választani 1-től 10-ig? ");
+
+                szoba_szam = Convert.ToInt32(Console.ReadLine());
+
+                int lehetosegek = 0;
+
+                while (szoba_szam < 1)
+                {
+                    lehetosegek++;
+                    Console.WriteLine("Vendéglátó: Az egyetlen probléma ezzel a választással, hogy az a szoba nem létezik.");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Vendéglátó: Tehát, melyik szobát szeretnéd választani 1-től 10-ig? ");
+
+                    szoba_szam = Convert.ToInt32(Console.ReadLine());
+
+
+
+                    if (lehetosegek == 2)
+                    {
+                        Console.WriteLine("Vendéglátó: Na jó, úgylátszik nem értetted a feladatot ezért én választom neked a 7-es szobát");
+                        szoba_szam = 7;
+                        break;
+                    }
+                }
+            }       
+
+            if(szoba_szam <= 10)
+            {
+                Console.WriteLine("Vendéglátó: Tessék a kulcs");
+                Thread.Sleep(1000);
+                Console.WriteLine("Vendéglátó: Jó pihenést");
+            }
 
         }
     }
