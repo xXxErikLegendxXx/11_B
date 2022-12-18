9ccfa11d7d91d666ef686c2c3f33b38437e339e0
Erikm0 <kis.erik.roland@blathy.info>
2022-12-18 15:44:43 +0100

    jatek

---
+++ jatek/jatek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace jatek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //üdvözlés
+
+            Console.WriteLine("Narrátor: Üdvözlet utazó, mond, hogy hívnak?");
+            string nev = Convert.ToString(Console.ReadLine());
+            Console.WriteLine($"Narrátor: Örülök, hogy megismerhettelek {nev}");
+        }
+    }
+}
